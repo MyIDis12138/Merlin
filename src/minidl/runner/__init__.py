964e8d94c1a7e43c49c7396387d22467058b535b
MyIDis12138 <yanggu123138@gmail.com>
2025-04-13 00:16:23 -0400

    optimized API, remove stale runner and update interface for checkpoints load and save

--- src/minidl/runner/__init__.py
+++ src/minidl/runner/__init__.py
@@ -1,12 +1,11 @@
 from .base_runner import BaseRunner
-from .epoch_based_runner import EpochBasedRunner, EpochBasedTrCudaRunner, MultimodalRunner
+from .epoch_based_runner import EpochBasedRunner, MultimodalRunner
 from .hooks import CheckpointSaverHook, EarlyStoppingHook, Hook, HookBuilder, HookRegistry, TensorboardLoggerHook, WandbLoggerHook
 from .runner_registry import RunnerBuilder, RunnerRegistry
 
 __all__ = [
     "BaseRunner",
     "EpochBasedRunner",
-    "EpochBasedTrCudaRunner",
     "MultimodalRunner",
     "Hook",
     "TensorboardLoggerHook",
